4e8ab7ce1ead4ee1c50e5ca146a7aa01b5568996
norman.katwesigye <norman.katwesigye@kittyhawk.aero>
2020-07-03 14:16:08 -0700

    enable database url variable

--- models.py
+++ models.py
@@ -15,7 +15,7 @@ db = SQLAlchemy()
 '''
     binds a flask application and a SQLAlchemy service
 '''
-def setup_db(app, database_path=database_path):
+def setup_db(app, database_path=database_url):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
